31a23c283a80c18311821a8ec04792379ab024c6
rudrbgus <rudrbgus3@gmail.com>
2024-03-06 16:19:15 +0900

    # 메인 페이지 초기설정
    라이브러리설치: react-router-dom
    Menu js 생성
    MainTemplate js 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainTemplate from "./page/MainTemplate";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainTemplate/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/page/MainTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainTemplate = () => {
+    return (
+        <div>
+            약체 코스프레
+        </div>
+    );
+};
+
+export default MainTemplate;
